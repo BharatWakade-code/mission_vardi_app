7db2b04692db690c3cbe956ca0e3d6cdcfd27c4e
Bharat Wakade <155426524+BharatWakade-code@users.noreply.github.com>
2026-06-01 22:22:00 +0530

    added ci cd

--- mission_vardi_backend/ecosystem.config.js
+++ mission_vardi_backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "mission-vardi-backend",
-      script: ".venv/Scripts/uvicorn.exe",
+      script: ".venv/bin/uvicorn",
       args: "app.main:app --host 0.0.0.0 --port 8000",
       interpreter: "none",
       instances: 1,
