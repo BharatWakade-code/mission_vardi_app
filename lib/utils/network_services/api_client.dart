6d73eba056be88b5d4cc48c41d2a5c2b1bc288ad
Bharat Wakade <155426524+BharatWakade-code@users.noreply.github.com>
2026-07-17 14:45:20 +0530

    Update BASE_URL configuration method in ApiClient
    Revert to using dotenv for BASE_URL configuration.

--- lib/utils/network_services/api_client.dart
+++ lib/utils/network_services/api_client.dart
@@ -6,11 +6,11 @@ class ApiClient {
   late final Dio dio;
 
   ApiClient() {
-    //final baseUrl = dotenv.env['BASE_URL'] ?? '';
-    const baseUrl = String.fromEnvironment(
-  'BASE_URL',
-  defaultValue: 'https://api.example.com',
-);
+    final baseUrl = dotenv.env['BASE_URL'] ?? '';
+//     const baseUrl = String.fromEnvironment(
+//   'BASE_URL',
+//   defaultValue: 'https://api.example.com',
+// );
     dio = Dio(
       BaseOptions(
         baseUrl: baseUrl,
